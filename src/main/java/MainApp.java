92ceb82ab5e66de8ffdced1f10be6a774a99d92e
manuela-ch <chirilamanuela@gmail.com>
2024-09-02 20:50:10 +0300

    insert method completed for Book

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,8 +1,13 @@
 import util.LibraryCli;
 
+import static util.LibraryCli.displayAllGenres;
+
 public class MainApp {
     public static void main(String[] args) {
         LibraryCli library = new LibraryCli();
         library.startLibraryCli();
+
+
+
     }
 }
