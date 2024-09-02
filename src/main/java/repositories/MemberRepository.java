e80f31abe98b968043c95a46b97df898d94cb51b
manuela-ch <chirilamanuela@gmail.com>
2024-09-02 19:00:19 +0300

    repository methods added for Member and Book

--- src/main/java/repositories/MemberRepository.java
+++ src/main/java/repositories/MemberRepository.java
@@ -1,4 +1,60 @@
 package repositories;
 
+import entities.Book;
+import entities.Member;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
 public class MemberRepository {
+    public void saveMember (Member member) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Transaction transaction = session.getTransaction();
+
+        session.save(member);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public void updateMember (Member member) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Transaction transaction = session.getTransaction();
+
+        session.update(member);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public Member findMemberById(int id){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Member member = session.get(Member.class,id);
+
+        session.getTransaction().commit();
+        session.close();
+
+        return member;
+    }
+
+    public void deleteMember(int id){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Member member = findMemberById(id);
+        if(member != null) {
+            session.delete(member);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
 }
